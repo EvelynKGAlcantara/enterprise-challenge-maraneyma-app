ccc3d9842894c5a0ec3741b1bae98dd39dcb14d7
Evelyn Karoline Gusson Alcântara <evelynkgalcantara@gmail.com>
2025-09-18 15:10:24 -0300

    [Feat] Tela Inicial

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,14 @@ import { Image, StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Image source={require("./logo-maraneyma.png")} />
+      <Image style={styles.image} source={require("./logo-maraneyma.png")} />
       <Image source={require("./pular-corda.png")} />
-      <Text style={styles.text}>Seu guia de</Text>
-      <Text style={styles.text}>esportes escolates</Text>
+      <Text style={styles.title}>Seu guia de</Text>
+      <Text style={styles.titleBold}>esportes escolares</Text>
       <Text style={styles.text}>
-        Maraneyma” significa saúde em Tupi-Guarani.
+        <Text style={styles.textBoldPurple}>"Maraneyma”</Text> significa
+        <Text style={styles.textBoldBlack}> "saúde"</Text> {"\n"} em{" "}
+        <Text style={styles.textBoldBlack}>Tupi-Guarani.</Text>
       </Text>
     </View>
   );
@@ -21,7 +23,30 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#D3D3D3",
   },
-  text: {
+  title: {
+    marginTop: 40,
+    fontSize: 30,
     color: "#000000",
+  },
+  titleBold: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  text: {
+    textAlign: "center",
+    marginTop: 20,
+    color: "#000000",
+    fontSize: 16,
+  },
+  textBoldPurple: {
+    fontWeight: "bold",
+    color: "#EB2F96",
+  },
+  textBoldBlack: {
+    fontWeight: "bold",
+    color: "#000000",
+  },
+  image: {
+    marginBottom: 30,
   },
 });
